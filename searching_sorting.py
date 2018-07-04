0cede383cca95da97941bf8463b6f5baea0cfa80
Uday Keith <udaykeith@gmail.com>
2018-07-04 12:17:32 +0530

    Update searching_sorting.py

--- searching_sorting.py
+++ searching_sorting.py
@@ -14,29 +14,7 @@ testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
 #print(sequentialSearch(testlist, 3))
 #print(sequentialSearch(testlist, 13))
 
-"""
-def binarySearch(alist, item):
-	first = 0
-	last = len(alist)-1
-	found = False
-	
-	while first<=last and not found:
-	    midpoint = (first + last)//2
-	    if alist[midpoint] == item:
-	        found = True
-	    else:
-	        if item < alist[midpoint]:
-	            last = midpoint-1
-	        else:
-	            first = midpoint+1
-	
-	return found
-also_list = sorted([1, 2, 32, 8, 17, 19, 42, 13, 0])
-print(binarySearch(testlist, 3))
-print(binarySearch(testlist, 13))
 
-#print(binarySearch(also_list, 13))
-"""
 def bubbleSort(alist):
 	for passnum in range(len(alist)-1,0,-1):
 		for i in range(passnum):
